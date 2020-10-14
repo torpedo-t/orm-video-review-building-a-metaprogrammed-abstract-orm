c215dd87a06e1f57892f7f1de45a93537329f798
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2020-10-14 14:09:39 -0500

    Done.

--- object_relational_mapping.rb
+++ object_relational_mapping.rb
@@ -44,12 +44,46 @@ class Orm
         ATTRIBUTES.collect{|attribute_name, schema| "#{attribute_name} #{schema}"}.join(", ")
     end
 
-    
-    end
-
     def self.create_table
         sql = <<-SQL
         CREATE TABLE IF NOT EXISTS #{self.table_name} (
-
+        #{self.create_sql}
         )
+        SQL
+
+        DB[:conn].execute(sql)
+    end
+
+    def insert 
+        sql = <<-SQL
+        INSERT INTO #{self.class.table_name} (#{self.class.attributes_for_insert}) VALUES (#{self.class.question_marks_for_insert})
+        SQL
+
+        DB[:conn].execute(sql, *attribute_values)
+        self.id = DB[:conn].execute("SELECT last_insert_rowid();").flatten.first
+    end
+
+    def self.attributes_for_insert
+        ATTRIBUTES.keys[1..-1].join(",")
+    end
+
+    def self.question_marks_for_insert
+        (ATTRIBUTES.keys.size-1).times.collect{"?"}.join(",")
+    end
+
+    def attribute_values
+        ATTRIBUTES.keys[1..-1].collect{|key| self.send(key)}
+    end
+
+    def self.sql_for_update
+        ATTRIBUTES.keys[1..-1].collect{|attribute_name| "#{attribute_name} = ?"}.join(",")
+    end
+    
+    def update 
+        sql = <<-SQL
+        UPDATE #{self.table_name} SET #{self.class.sql_for_update} WHERE id = ?
+        SQL
+
+        DB[:conn].execute(sql, *attribute_values, self.id)
+    end
 end
